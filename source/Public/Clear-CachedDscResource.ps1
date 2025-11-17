623522eee75b43af69deb8b39e2214c2f4de923b
Raimund Andree <r.andree@live.com>
2025-11-17 15:06:01 +0100

    Refactor Clear-CachedDscResource function for improved readability and logic flow

--- source/Public/Clear-CachedDscResource.ps1
+++ source/Public/Clear-CachedDscResource.ps1
@@ -4,26 +4,30 @@ function Clear-CachedDscResource
     [CmdletBinding(SupportsShouldProcess = $true)]
     param ()
 
+    Write-Verbose 'Stopping any existing WMI processes to clear cached resources.'
+
+    ### find the process that is hosting the DSC engine
+    $dscProcessID = Get-WmiObject msft_providers |
+        Where-Object { $_.provider -like 'dsccore' } |
+            Select-Object -ExpandProperty HostProcessIdentifier
+
+    ### Stop the process
+    if ($dscProcessID)
+    {
+        Get-Process -Id $dscProcessID | Stop-Process
+    }
+    else
+    {
+        Write-Verbose 'Skipping killing the DSC Process'
+    }
+
     if ($pscmdlet.ShouldProcess($env:computername))
     {
-        Write-Verbose 'Stopping any existing WMI processes to clear cached resources.'
-
-        ### find the process that is hosting the DSC engine
-        $dscProcessID = Get-WmiObject msft_providers |
-            Where-Object { $_.provider -like 'dsccore' } |
-                Select-Object -ExpandProperty HostProcessIdentifier
-
-        ### Stop the process
-        if ($dscProcessID -and $PSCmdlet.ShouldProcess('DSC Process'))
-        {
-            Get-Process -Id $dscProcessID | Stop-Process
-        }
-        else
-        {
-            Write-Verbose 'Skipping killing the DSC Process'
-        }
-
         Write-Verbose 'Clearing out any tmp WMI classes from tested resources.'
         Get-DscResourceWmiClass -Class tmp* | Remove-DscResourceWmiClass
+    }
+    else
+    {
+        Write-Verbose 'Skipping clearing out any tmp WMI classes from tested resources.'
     }
 }
